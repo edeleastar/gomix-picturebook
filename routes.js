bf0a8a699a601d0c48ac11c024ee775c3b0d5bd6
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-23 07:59:18 +0000

    package updates

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const start = require('./controllers/start');
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
 const accounts = require('./controllers/accounts.js');
+const pictureStore = require('./models/picture-store.js');
 
 router.get('/', accounts.index);
 router.get('/login', accounts.login);
@@ -17,5 +18,9 @@ router.post('/authenticate', accounts.authenticate);
 
 router.get('/dashboard', dashboard.index);
 router.get('/about', about.index);
+router.get('/dashboard/getpicture/:path/:name', dashboard.getPicture);
+router.post('/dashboard/uploadpicture', dashboard.uploadPicture);
+
+pictureStore.init();
 
 module.exports = router;
